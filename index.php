d4b26b2ce3463428ba12bff5d48cfa13437892a4
kristofgasiciel <krzysztof.szarski@o2.pl>
2022-09-13 10:30:52 +0200

    zad2.1

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 $content = file_get_contents('http://loripsum.net/api');
 $test = explode(" ", $content);
-var_dump($test);
+#var_dump($test);
+$i = 1;
+foreach($test as $slowo){
+    echo "[$i] $slowo </br>";
+    if(preg_match('/\b(\w*e\w*)\b/', $slowo, $match) == true){
+    echo "[$i] $match[0] </br>";
+    $i ++;
+    }
+}
+
+#$znaki=$_POST["znaki"];
+#function sortowanie($znaki, $test){
+#    natcasesort($test): bool
+#};
 ?>
